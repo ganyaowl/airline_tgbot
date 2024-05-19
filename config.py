e4b1ce461cb425cde5473686e0cad71ef77d9858
Ganijon <146222408+ganyaowl@users.noreply.github.com>
2024-05-19 11:00:23 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from aiogram.types import LabeledPrice
 
-TOKEN = "6877052402:AAGXgWf5LwLlwg4qsvwU9bF-2kFPifwR1rA"
+TOKEN = "Your token"
 
-PAYMENT_TOKEN = "398062629:TEST:999999999_F91D8F69C042267444B74CC0B3C747757EB0E065"
+PAYMENT_TOKEN = "Payment token"
 ECONOMY_PRICE = LabeledPrice(label="Оплата за билет", amount=499_900*100) # считает в копейках, поэтому умножаем на 100
 BUSINESS_PRICE = LabeledPrice(label="Оплата за билет", amount=999_900*100)
 
